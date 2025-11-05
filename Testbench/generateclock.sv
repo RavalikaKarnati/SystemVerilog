f64e3abc77c22792bb89f1282c87bf52610c117c
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-05 12:56:57 -0500

    Create generateclock.sv

---
+++ Testbench/generateclock.sv
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+ 
+module tb();
+ 
+  
+  reg clk; //initial value = X
+  
+  reg clk50;
+  reg clk25 = 0;  ///initialize variable
+  
+ 
+  initial begin
+    clk = 1'b0;
+    clk50 = 0; 
+  end
+  
+  always #5 clk = ~clk;
+  
+// generate clocks with phase shift
+// always #10 clk50 = ~clk50;
+// always #20 clk25 = ~clk25;
+ 
+ // generate clocks without phase shift
+  always begin
+  #5;
+   clk50 = 1;
+  #10;
+   clk50 = 0; 
+   #5; 
+  end
+  
+  always begin
+    #5;
+    clk25 = 1;
+    #20;
+    clk25 = 0;
+    #15;  
+  end
+ 
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+ 
+ 
+  initial begin
+    #200;
+    $finish();
+  end
+  
+endmodule
