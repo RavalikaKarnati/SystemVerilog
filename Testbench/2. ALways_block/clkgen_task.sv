896a0c882054d150591a35da37a30974ebfac5c4
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-08 10:04:20 -0500

    Update clkgen_task.sv

--- Testbench/2. ALways_block/clkgen_task.sv
+++ Testbench/2. ALways_block/clkgen_task.sv
@@ -1,42 +1,56 @@
+/////////////////////////////////////////// Method-1 : generating Clock ///////////////////////////////////////
 `timescale 1ns / 1ps
  
+module tb();
+  reg clk = 0; 
+  reg clk50 = 0;
+  real phase_diff = 10;
+  real ton = 5;
+  real toff = 5; 
+ 
+  always #5 clk = ~clk; //100 MHz --> 10ns
+ 
+  task clkgen(input real phase, input real ton, input real toff);  
+    #phase_diff;
+    while(1) begin
+    clk50 = 1;
+    #ton;
+    clk50 = 0;
+    #toff;
+    end
+  endtask
+*/
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end 
+  initial begin
+    #200;
+    $finish();
+  end
+endmodule
+
+/////////////////////////////////////////// Method-2 : generating Clock ///////////////////////////////////////
+ 
+`timescale 1ns / 1ps
 module tb();
  
   
   reg clk = 0; 
   reg clk50 = 0;
   
-  always #5 clk = ~clk; //100 MHz
-    
-  /*
-  real phase = 10;
-  real ton = 5;
-  real toff = 5;
-  */
-/*  
-task clkgen(input real phase, input real ton, input real toff);  
-  #phase;
-  while(1) begin
-  clk50 = 1;
-  #ton;
-  clk50 = 0;
-  #toff;
-  end
-endtask
-*/
+  always #5 clk = ~clk; //100 MHz 
+  task calc (input real freq_hz, input real duty_cycle, input real phase, output real pout, output real ton, output real toff);
+    pout = phase;
+    ton = (1.0 / freq_hz) * duty_cycle * 1000_000_000;
+    toff =  (1000_000_000 / freq_hz) - ton;
+  endtask
  
  
-task calc (input real freq_hz, input real duty_cycle, input real phase, output real pout, output real ton, output real toff);
-pout = phase;
-ton = (1.0 / freq_hz) * duty_cycle * 1000_000_000;
-toff =  (1000_000_000 / freq_hz) - ton;
-endtask
- 
- 
-task clkgen(input real phase, input real ton, input real toff);  
-  @(posedge clk);
-  #phase;
-  while(1) begin
+  task clkgen(input real phase, input real ton, input real toff);  
+    @(posedge clk);
+    #phase;
+    while(1) begin
   clk50 = 1;
   #ton;
   clk50 = 0;
