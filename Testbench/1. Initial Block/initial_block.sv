2619758b6a91b42a4c2c89ef5f2aaf36a9a3071e
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-08 09:34:57 -0500

    Update initial_block.sv

--- Testbench/1. Initial Block/initial_block.sv
+++ Testbench/1. Initial Block/initial_block.sv
@@ -15,12 +15,21 @@ module tb();
     rst = 1'b0;
   end
   ////2. Random signal for data/ control 
-  
+
+
+ // the initial block starts its execution at the start of a simulation and will be executing the sequence that you have mentioned inside this block right from 0ns.
+ // As soon as we execute all the statements that you mentioned,  this will complete the execution of the initial block. And then, as soon as we complete the execution of all the statements,
+// that basically completes the span of the initial block. but when we work with the reg type, this basically holds the value. So, even though our execution finishes
+//over here, for example, if we consider below initial block: rst = 1 from 0 to 30 ns, and after a delay of 30,
+//we are making rst = 0. so at this instance, we complete the execution of the initial block, but the variable will still hold its value till the end of the simulation. 
+// since this is the last statement that we have in the initial block, it will still hold its value till the end of the simulation due to the reg data type. 
+// Reg has memory that keeps the last value till the end of the simulation. 
+ 
   initial begin
     rst = 1'b1;
     #30;
-    rst = 1'b0;
-  end
+   rst = 1'b0;  // this will hold the value to the end of simulation as this is the last statement and rst is of "reg" type.
+ end
   
   
   initial begin
