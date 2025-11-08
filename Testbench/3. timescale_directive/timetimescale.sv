3009cc66914686e93d6374c2d3ec560e9c859634
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-08 10:14:06 -0500

    Update timetimescale.sv

--- Testbench/3. timescale_directive/timetimescale.sv
+++ Testbench/3. timescale_directive/timetimescale.sv
@@ -1,29 +1,29 @@
-`timescale 1ns / 1ps   //10^3 -> 3
-// if `timescale 1ns / 1ns // 10^0 -> 0
+`timescale 1ns / 1ps  
+// `timescale __timeunit________ / _____timeprecision_____
+//  `timescale 1ns / 1ps   --> 1ns --> TimeUnit
+//                         --> 1ps --> TimePrecison
+//  `timescale 1ns / 1ps --> T. U / T.P = 1 ns/ 1ps = 1000 = 10^3 , so here "3" is the power for a ten that represent the number of precision digit
+
+// if `timescale 1ns / 1ns = 1 = 10^0 = 0 so here "0" is the power for a ten that represent the number of precision digit
+
 // in this case #31.25 mentioned below for 16MHz will round off to 31
 // in this case #62.5 mentioned below for 8MHz will round off to 63
- 
+
 module tb();
  
-
   reg clk16 = 0;
   reg clk8 = 0;  ///initialize variable
   
- 
-   always #31.25 clk16 = ~clk16;
-   always #62.5 clk8 = ~clk8;
-  
- 
- 
+  always #31.25 clk16 = ~clk16;
+  always #62.5 clk8 = ~clk8;
+
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars;
   end
  
- 
   initial begin
     #200;
     $finish();
-  end
-  
+  end 
 endmodule
