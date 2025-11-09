33247e906bf29f0cb23e613b93c38d366cf3811d
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-09 12:03:34 -0500

    Update function.sv

--- Testbench/7. SV OOP Construct/function.sv
+++ Testbench/7. SV OOP Construct/function.sv
@@ -93,22 +93,42 @@ endmodule
 // OUTPUT
 // KERNEL: ain : 3 and bin : 7
 
-///////////////////////////////////////////////////////////// FUNCTION definition WITH ////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////// FUNCTION definition WITH INPUT AND OUTPU PORTS ////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Although ANSI-C style declaration was later introduced in Verilog, the old style declaration of port directions are still valid. 
+// SystemVerilog functions can have arguments declared as input and output ports as shown in the example below.
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 module tb;
-
-  bit [4:0] result; 
-  bit [3:0] ain = 4'b0011;
-  bit [3:0] bin = 4'b0111;
-  
-  function void display_ain_bin();
-    $display(" ain : %0d and bin : %0d", ain, bin);   
-  endfunction
-
-  initial begin
-    display_ain_bin();
+	initial begin
+      	int res, s;
+      	s = sum(5,9);
+    	$display ("s = %0d", sum(5,9));
+      	$display ("sum(5,9) = %0d", sum(5,9));
+      	$display ("mul(3,1) = %0d", mul(3,1,res));
+      	$display ("res = %0d", res);
   end
+
+	// Function has an 8-bit return value and accepts two inputs
+  	// and provides the result through its output port and return val
+  function bit [7:0] sum;
+      	input int x, y;
+      	output sum;
+		sum = x + y;
+	endfunction
+
+  	// Same as above but ports are given inline
+  function byte mul (input int x, y, output int res);
+    	res = x*y + 1;
+    	return x * y;
+  endfunction
 endmodule
 
 // OUTPUT
-// KERNEL: ain : 3 and bin : 7
+//s = 14
+//sum(5,9) = 14
+//mul(3,1) = 3
+//res = 4
+//ncsim: *W,RNQUIE: Simulation is complete.
 
