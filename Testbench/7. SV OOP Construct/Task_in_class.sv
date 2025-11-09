d043a6922fe49cc9036d0f1389940d0367b560d6
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-09 17:03:59 -0500

    Update Task_in_class.sv

--- Testbench/7. SV OOP Construct/Task_in_class.sv
+++ Testbench/7. SV OOP Construct/Task_in_class.sv
@@ -1,1 +1,27 @@
 /////////////////////////////////////////////////////////////////////// TASK IN CLASS ///////////////////////////////////////////////////////////////////
+class custom
+  int data1;
+  bit [7:0] data2;
+  shortint data3;
+  
+  function new(input int data1 = 0, input bit [7:0] data2 =0; input shortint data3 = 0); 
+    this.data1 = data1;      //  all the three arguments which are there in a constructor are given the same name, which are used to name our data members.
+    this.data2 = data2;      // keyword = "this." to refer to the datamember of the class
+    this.data3 = data3; 
+  endfunction
+
+  task display();
+     $display("data1 : %0d, data2 : %0d, data3 : %0d", f.data1, f.data2, f.data3);
+  endtask
+endclass
+
+module tb;
+custom f;
+ initial begin
+   f = new(.data2(4), .data3(5), .data1(6)); // follow by the position -> 11 is assigned to data1, 22 is assigned to data2 , 33 is assigned to data3
+   f.display();
+ end
+endmodule
+
+// OUTPUT
+// data1 : 6, data2 : 4, data3 : 5
