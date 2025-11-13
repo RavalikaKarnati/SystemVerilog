f4eb8199480d49d16d4c40fcc114a959b323dd82
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-12 21:04:14 -0500

    Update polymorphsim.sv

--- Testbench/7. SV OOP Construct/polymorphsim.sv
+++ Testbench/7. SV OOP Construct/polymorphsim.sv
@@ -40,7 +40,7 @@ endmodule
 // here we have method display() in both parent class and extended class so, in this case display() method in parent class will be overridden with the extended class and the display() in extended class is executed
 // if If you do not override the display() method ( or donot define display() method) in the extended class, in that case, the method that we have in the parent class will be executed
 
-// Example: // here we have method display() in both parent class and extended class so, in this case display() method in parent class will be overridden with the extended class and 
+// Example1: // here we have method display() in both parent class and extended class so, in this case display() method in parent class will be overridden with the extended class and 
              // the display() in extended class is executed
 
 class first;           /// PARENT CLASS ////////
@@ -76,7 +76,7 @@ endmodule
 # KERNEL: SECOND: value of data: 12 and temp :34
 
 -------------------------------------------------------------------------------------------------------
-// Example: If you do not override the display() method ( or donot define display() method) in the extended class, in that case, the method that we have in the parent class will be executed
+// Example2: If you do not override the display() method ( or donot define display() method) in the extended class, in that case, the method that we have in the parent class will be executed
 
 class first;           /// PARENT CLASS ////////
   int data =12;
@@ -109,3 +109,9 @@ endmodule
 
 //OUTPUT
 # KERNEL: FIRST:value of data: 12
+
+/// So our method display has multiple behaviors. If you do not modify the method,it behaves as Example2. If you modify the method, it behaves as Example1. This is refer to as "POLYMORPHISM".
+//  In the parent class, all the methods will be declared as virtual. So if the user does not modify the method, the method that we have in the parent class will be executed. Otherwise, the method that you have
+// in an extended class will be executed, and this is what we require to build complex expressions. So in a case where we want to modify our expression depending on the stimulus that we are
+// generating, we could just modify our method and generate the complex stimulus
+
