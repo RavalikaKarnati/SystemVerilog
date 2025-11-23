b6787cd2355a735fc1ee327e47a84f66a8de720a
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-22 23:16:48 -0500

    Update forkJoinDemo.sv

--- Testbench/9. IPC/forkJoinDemo.sv
+++ Testbench/9. IPC/forkJoinDemo.sv
@@ -1,3 +1,4 @@
+/////////////////////////////////////////////////////////////////////// fork--- join ///////////////////////////////////////////////////////////////////////////////////////
 module tb;
   task first();
     $display("Task1 started at %0t", $time);
@@ -23,3 +24,49 @@ module tb;
     third();
   end
 endmodule
+
+//output
+# KERNEL: Task1 started at 0
+# KERNEL: Task2 started at 0
+# KERNEL: Task1 completed at 20
+# KERNEL: Task2 completed at 30
+# KERNEL: reached next to join at 30
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
+
+
+/////////////////////////////////////////////////////////////////////// fork--- join_any ///////////////////////////////////////////////////////////////////////////////////////
+module tb;
+  task first();
+    $display("Task1 started at %0t", $time);
+    #20;
+    $display("Task1 completed at %0t", $time);
+  endtask
+
+  task second();
+    $display("Task2 started at %0t", $time);
+    #30;
+    $display("Task2 completed at %0t", $time);
+  endtask
+
+  task third();
+    $display("reached next to join at %0t", $time);
+  endtask
+
+  initial begin
+    fork
+      first();
+      second();
+    join_any
+    third();
+  end
+endmodule
+
+//output
+# KERNEL: Task1 started at 0
+# KERNEL: Task2 started at 0
+# KERNEL: Task1 completed at 20
+# KERNEL: reached next to join at 20
+# KERNEL: Task2 completed at 30
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
