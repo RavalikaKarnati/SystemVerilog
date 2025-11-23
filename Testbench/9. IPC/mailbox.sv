13902351561448e17c0662dc37294e53551615c6
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-11-23 17:43:36 -0500

    Update mailbox.sv

--- Testbench/9. IPC/mailbox.sv
+++ Testbench/9. IPC/mailbox.sv
@@ -1,1 +1,35 @@
+//  mailbox --  how to send data between classes. generator and driver, where we have a single data member that we want to send between the classes,
+// mailbox is a mechanism by which we communicate data between generator and driver, and between monitor and scoreboard
 
+class generator;
+  int data = 12;
+  mailbox gen2drv_mbx; // gen2drv
+  task run();
+    gen2drv_mbx.put(data);
+    $display("[GEN] : SENT DATA : %0d", data); //  we follow UVM-like terminology, We can add a tag
+  endtask
+endclass
+
+class driver;
+  int datac = 0;
+  mailbox drv2gen_mbx;
+  task run();
+    drv2gen_mbx.get(datac);
+    $display("[DRV] : RCVD DATA : %0d", datac);
+  endtask
+endclass
+
+module tb;
+  generator gen;
+  driver drv;
+  mailbox mbx;
+
+  initial begin
+    gen = new();
+    drv = new();
+    gen.gen2drv_mbx = mbx;  // common for both generator and driver
+    drv.drv2gen_mbx = mbx;
+    gen.run();
+    drv.run();
+  end
+endmodule
