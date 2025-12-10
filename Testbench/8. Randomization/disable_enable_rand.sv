6382aecddf6fd9b80893b8792e657417d3d3a35b
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-12-10 18:51:38 -0500

    Disable randomization for all variables in g1
    Added a line to disable randomization for all variables in the class.

--- Testbench/8. Randomization/disable_enable_rand.sv
+++ Testbench/8. Randomization/disable_enable_rand.sv
@@ -12,6 +12,8 @@ module tb;
     g1 = new();
     g1.a.rand_mode(0); // disable randomization for variable "a"
     g1.b.rand_mode(1); // enable randomization for variable "b"
+    // Turns off randomization for all variables
+    g1.rand_mode (0);
     for ( int i=0; i<10; i++) begin  // repeat (10) or foreach ( size)
       status = g1.randomize();    //  randomize(); --> This will generate the random value for the signals modified with the keyword rand, both a and b are modified with the rand keyword, so for a and b, we will generate random values
                                   // To check whether randomization is successful or not, we use status. If the randomization is successful, status is "1" else status is "0"
